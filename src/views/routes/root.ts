40c967ddf3aa4fa3a84925a1e5f6d750bc1bafb6
me-dangnhatminh <dangnhatminh09032002@gmail.com>
2024-03-07 14:57:54 +0700

    Add joy ui lib

--- src/views/routes/root.ts
+++ src/views/routes/root.ts
@@ -2,15 +2,11 @@ import { lazy } from "react";
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 
 enum RoutesPath {
-  HOME = "/home",
+  HOME = "/",
   LOGIN = "/login",
 }
 
 const routes: RouteObject[] = [
-  {
-    path: "/",
-    Component: lazy(() => import("../pages/HomePage")),
-  },
   {
     path: RoutesPath.HOME,
     Component: lazy(() => import("../pages/HomePage")),
@@ -36,19 +32,8 @@ const errorRoutes: RouteObject[] = [
   },
 ];
 
-const privateRoutes: RouteObject[] = [
-  {
-    path: "/admin",
-    Component: lazy(() => import("../pages/AdminPage")),
-  },
-];
-
-function getRoutes(role: "admin" | "user") {
-  const _routes = [...routes];
-  if (role === "admin") _routes.push(...privateRoutes);
-  _routes.push(...errorRoutes);
-
-  return _routes;
+function getRoutes() {
+  return [...routes, ...errorRoutes];
 }
 
-export const rootRouter = createBrowserRouter(getRoutes("admin"));
+export const rootRouter = createBrowserRouter(getRoutes());
